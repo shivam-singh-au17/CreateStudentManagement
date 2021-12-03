f98f99c01c15e36328b53da919b95c0fb36e7be4
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-03 20:39:01 +0530

    client side file

---
+++ client/src/components/LoginPage.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+
+const iniState = {
+  userUserEmail: "",
+  userPassword: "",
+};
+
+const LoginPage = () => {
+  const [helpData, setHelpData] = useState(iniState);
+
+  const handleChang = (e) => {
+    const { name, value, type, checked } = e.target;
+    const myAllUserData = {
+      ...helpData,
+      [name]: type === "checkbox" ? checked : value,
+    };
+    setHelpData(myAllUserData);
+  };
+
+  const submitHelpPopup = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      body: JSON.stringify(helpData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setHelpData(iniState);
+    alert("Submited");
+  };
+
+  const { userUserEmail, userPassword } = helpData;
+
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col">
+          <img
+            className="my-5"
+            src="https://learn.masaischool.com/img/login-page.svg"
+            alt=""
+          />
+        </div>
+
+        <div className="col">
+          <h1 className="text-center my-5">Login Form</h1>
+          <div className="mb-3">
+            <label htmlFor="myInput1" className="form-label">
+              Email address
+            </label>
+            <input
+              name="userUserEmail"
+              value={userUserEmail}
+              onChange={handleChang}
+              type="email"
+              className="form-control"
+              id="myInput1"
+              placeholder="Enter Email"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="myInput2" className="form-label">
+              Password
+            </label>
+            <input
+              name="userPassword"
+              value={userPassword}
+              onChange={handleChang}
+              type="email"
+              className="form-control"
+              id="myInput2"
+              placeholder="Enter Password"
+            />
+          </div>
+          <button
+            onClick={submitHelpPopup}
+            className="btn btn-primary my-3"
+            style={{ float: "right" }}
+          >
+            SignUp
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
