b4b990592b20ebe88a529c23d9305a77944ba197
Shivam Singh <shivamsingh4458@gmail.com>
2021-12-04 08:20:52 +0530

    client side files

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,17 @@ import AllConttestList from './components/AllConttestList';
 import AdminPannel from './components/AdminPannel';
 import LoginPage from './components/LoginPage';
 import Navbar from './components/Navbar';
+import UserNavbar from './components/UserNavbar';
 import { LoginContext } from './components/LoginContext';
 
 
 function App() {
-  const { role } = useContext( LoginContext );
-  console.log( 'role:', role )
+  const { role } = useContext(LoginContext);
+  console.log('role:', role)
   return (
     <div className="App">
-      { role === "admin" ? <>
-        <AdminPannel /></> : role === "user" ? <> <Navbar /> <AllConttestList /></> : <><Navbar /> <LoginPage /> </> }
+      {role === "admin" ? <>
+        <AdminPannel /></> : role === "user" ? <> <UserNavbar /> <AllConttestList /></> : <><Navbar /> <LoginPage /> </>}
     </div>
   );
 }
